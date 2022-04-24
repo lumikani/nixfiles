2c3bf8b7abe7382905be06706d174e9184713506
Lumi <lumicake@pm.me>
2022-04-24 20:26:11 +0300

    Fiddle with i3 configuration

--- home/i3.nix
+++ home/i3.nix
@@ -2,15 +2,57 @@
 let
   mode_system = "(l)ock, (e) logout, (s) suspend, (h) hibernate, (r) reboot, (Shift+s) shutdown";
   lock_system = "i3lock -c 000000 && sleep 1";
+  i3status-bottom = "bottom";
 in
 {
   xsession.windowManager.i3 = {
     enable = true;
     config = {
+      bars = [
+        {
+          fonts = {
+            names = [ "Iosevka Term" ];
+            size = 12.0;
+          };
+          position = "bottom";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-${i3status-bottom}.toml";
+          colors = {
+            background = "#000000";
+            statusline = "#ffffff";
+            separator = "#666666";
+            focusedWorkspace = {
+              border = "#4c7899";
+              background = "#285577";
+              text = "#ffffff";
+            };
+            activeWorkspace = {
+              border = "#333333";
+              background = "#5f676a";
+              text = "#ffffff";
+            };
+            inactiveWorkspace = {
+              border = "#333333";
+              background = "#222222";
+              text = "#888888";
+            };
+            urgentWorkspace = {
+              border = "#2f343a";
+              background = "#900000";
+              text = "#ffffff";
+            };
+            bindingMode = {
+              border = "#2f343a";
+              background = "#900000";
+              text = "#ffffff";
+            };
+          };
+        }
+      ];
+
       modes = lib.mkOptionDefault {
         "${mode_system}" = {
           l = "exec --no-startup-id ${lock_system}, mode \"default\"";
-          e = "exec --no-startup-id i3-msg exit, mode \"default\"";
+          e = "exec --no-startup-id xfce4-session-logout, mode \"default\"";
           s = "exec --no-startup-id ${lock_system} && systemctl suspend, mode \"default\"";
           h = "exec --no-startup-id ${lock_system} && systemctl suspend, mode \"default\"";
           r = "exec --no-startup-id systemctl reboot, mode \"default\"";
@@ -30,7 +72,7 @@ in
       focus.followMouse = false;
       fonts = {
         names = [ "Iosevka Regular Term" ];
-        size = 14.0;
+        size = 11.0;
       };
 
       keybindings = let
@@ -56,6 +98,66 @@ in
       terminal = "alacritty";
 
       workspaceAutoBackAndForth = true;
+    };
+  };
+
+  home.packages = with pkgs; [
+    font-awesome_5 # needed by i3status-rust
+  ];
+
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      "${i3status-bottom}" = {
+        blocks = [
+          {
+            block = "pomodoro";
+            length = 25;
+            break_length = 10;
+            message = "✨ Focus timer done ✨";
+            notifier = "i3nag";
+            break_message = "✨ Chill timer done ✨";
+          }
+          {
+            block = "disk_space";
+            path = "/";
+            alias = "/";
+            info_type = "available";
+            unit = "GB";
+            interval = 60;
+            warning = 20.0;
+            alert = 10.0;
+          }
+          {
+            block = "memory";
+            display_type = "memory";
+            format_mem = "{mem_used_percents}";
+            format_swap = "{swap_used_percents}";
+          }
+          {
+            block = "cpu";
+            interval = 1;
+          }
+          {
+            block = "load";
+            interval = 1;
+            format = "{1m}";
+          }
+          { block = "sound"; }
+          { 
+            block = "battery";
+            interval = 30;
+            format = "{percentage} ({time})";
+          }
+          {
+            block = "time";
+            interval = 60;
+            format = "%a %F %R";
+          }
+        ];
+        icons = "awesome5";
+        theme = "space-villain";
+      };
     };
   };
 }
