2ac38091e710ce6630f23b58d42f9705185dfa4c
Lumi <kallioniemi@pm.me>
2022-12-05 15:43:28 +0200

    fixup! nvim: update deprecated `resolved_capabilities`

--- home/nvim.nix
+++ home/nvim.nix
@@ -249,7 +249,7 @@ in
             })
 
             -- Setup lspconfig.
-            local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
             require('lspconfig').pylsp.setup{
               capabilities = capabilities,
